43b68f4ef8f952d6edc63cf02c6c32cf80e1a215
MaSStiK <matvey.ovchinnikov.2014@mail.ru>
2023-04-22 17:09:20 +0200

    canvas.toDataURL changed

--- src/components/AvatarDownloader.vue
+++ src/components/AvatarDownloader.vue
@@ -2,7 +2,7 @@
 function download() {
     const canvas:any =  document.querySelector(".generator__canvas");
     (document.querySelector("#downloader") as any).download = "Lego Avatar.png";
-    (document.querySelector("#downloader") as any).href = canvas.toDataURL("image/png").replace(/^data:image\/[^;]/, 'data:application/octet-stream')
+    (document.querySelector("#downloader") as any).href = canvas.toDataURL("image/png")
 }
 </script>
 
